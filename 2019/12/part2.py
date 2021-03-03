d5e29fd6c370035b94f0acf2217d1c1339d6635b
Lannie <rsl.ligthart@gmail.com>
2021-03-03 20:01:39 +0100

    added 2019/12/2

--- 2019/12/part2.py
+++ 2019/12/part2.py
@@ -2,7 +2,7 @@ import itertools
 import numpy as np
 import copy
 
-with open('testinput.txt') as f:
+with open('input.txt') as f:
     data = f.read().split("\n")
 
 moons_position = []
@@ -35,12 +35,10 @@ def time_step(moons_position, moons_velocity):
     for c in combos:
         m1 = c[0]
         m2 = c[1]
-        #print(c[0], c[1])
         moons_velocity[m1],  moons_velocity[m2] = apply_gravity(moons_position[m1], moons_position[m2], moons_velocity[m1], moons_velocity[m2])
     moons_position = np.array(moons_position) + np.array(moons_velocity)
     moons_position = [list(x) for x in moons_position]
     return (moons_position, moons_velocity)
-
 
 def get_energy(moons_position, moons_velocity):
     pot = []
@@ -57,76 +55,87 @@ def get_energy(moons_position, moons_velocity):
     return sum(tot)
 
 
-# def compare(start_positions, moons_position, n):
-#     same = [[None, None, None], [None, None, None], [None, None, None], [None, None, None]]
-#     for i in range(len(start_positions)):
-#         # if start_positions[i] == moons_position[i]:
-#         #     same[i] = n
-#         for j in range(len(start_positions[i])):
-#             if start_positions[i][j] == moons_position[i][j]:
-#                 same[i][j] = n
-#     return same
-
 def compare(start_positions, start_velocities, moons_position, moons_velocity, n):
-    same = [None, None, None, None]
-    for i in range(len(start_positions)):
-        if start_positions[i] == moons_position[i] and start_velocities[i] == moons_velocity[i]:
-            same[i] = n
-            print(moons_position)
-            print(n)
+    same = [None, None, None]
+    spos_x = [x[0] for x in start_positions]
+    spos_y = [x[1] for x in start_positions]
+    spos_z = [x[2] for x in start_positions]
+    svel_x = [x[0] for x in start_velocities]
+    svel_y = [x[1] for x in start_velocities]
+    svel_z = [x[2] for x in start_velocities]
+    pos_x = [x[0] for x in moons_position]
+    pos_y = [x[1] for x in moons_position]
+    pos_z = [x[2] for x in moons_position]
+    vel_x = [x[0] for x in moons_velocity]
+    vel_y = [x[1] for x in moons_velocity]
+    vel_z = [x[2] for x in moons_velocity]
+    if spos_x == pos_x and svel_x == vel_x:
+        same[0] = n
+    if spos_y == pos_y and svel_y == vel_y:
+        same[1] = n
+    if spos_z == pos_z and svel_z == vel_z:
+        same[2] = n
     return same
-
 
 start_positions = copy.deepcopy(moons_position)
 start_velocities = copy.deepcopy(moons_velocity)
 
 n = 0
-periods = [[], [], [], []]
+periods = [[],[],[]]
 
 while True:
     n += 1
     moons_position, moons_velocity = time_step(moons_position, moons_velocity)
-    #print(moons_position, moons_velocity)
     same = compare(start_positions, start_velocities, moons_position, moons_velocity, n)
     for i in range(len(same)):
-        #for j in range(len(same[i])):
-            # if same[i][j] is not None and periods[i][j] is None:
-            #     periods[i][j] = n
         if same[i] is not None:
             periods[i].append(same[i])
             print(periods)
-#    if not None in periods[0] and not None in periods[1] and not None in periods[2]:
-    p_len = [len(periods[i]) for i in range(len(periods))]
+
+    p_len = []
+    for i in range(len(periods)):
+        p_len.append(len(periods[i]))
     if min(p_len) > 0:
        break
 
-# all_periods = periods[0]
-# all_periods.extend(periods[1])
-# all_periods.extend(periods[2])
-# all_periods.extend(periods[3])
-# print(all_periods)
-
-#
-# def merge(p1, p2):
-#     if p1 > p2:
-#         largest = p1
-#         smallest = p2
-#     elif p1 < p2:
-#         largest = p1
-#         smallest = p2
-#     increment = largest
-#     while True:
-#         if largest % smallest == 0:
-#             return largest
-#         largest += increment
-#
-#
-# #period = merge(all_periods[0], all_periods[1])
-# period = merge(periods[0], periods[1])
-#
-# for i in range(2, len(periods)):
-#     period = merge(period, periods[i])
-#     print(period)
+def merge(p1, p2):
+    if p1 == p2:
+        return p1
+    elif p1 > p2:
+        largest = p1
+        smallest = p2
+    elif p1 < p2:
+        largest = p1
+        smallest = p2
+    increment = largest
+    while True:
+        if largest % smallest == 0:
+            return largest
+        largest += increment
 
 
-# [924, 617, 2772, 924]
+def combine(moon_periods):
+    moon_periods = sorted(moon_periods)
+    numbers = moon_periods
+    n1 = numbers[0]
+    numbers = numbers[1:]
+    while len(numbers) > 0:
+        n1 = merge(n1, numbers[0])
+        numbers = numbers[1:]
+    return n1
+
+# bepaal de periode voor elk van de coordinaten (voor alle manen tegelijk)
+moon_periods = []
+for i in range(len(periods)):
+    moon_periods.append(periods[i][0])
+print(moon_periods)
+
+merged = []
+
+print(merged)
+merged = combine(moon_periods)
+
+print(merged)
+
+
+
