8f0fa6c91c7fb3dc4f1b029bbff1d01e29fd87a2
Lannie <rsl.ligthart@gmail.com>
2021-03-06 14:48:04 +0100

    some cleanup

--- 2019/13/part2.py
+++ 2019/13/part2.py
@@ -3,100 +3,65 @@ sys.path.append("C:/Users/Admin/Documents/Code/advent_of_code/2019/13")
 sys.path.append("C:/Users/Admin/Documents/Code/advent_of_code/AoC_tools")
 import IntCode as ic
 from aoc_tools import display_grid
+import time
 
 code = ic.parse_code('C:/Users/Admin/Documents/Code/advent_of_code/2019/13/input.txt')
 # play for free
 code[0] = 2
 prog = ic.Intcode(code)
 
-def read_instructions(instructions):
+def read_instructions(instructions, get_tiles=True):
     tiles = {}
     pos = 0
     while pos < len(instructions):
         x = instructions[pos]
         y = instructions[pos + 1]
         tile_id = instructions[pos + 2]
-        if not x == -1:
-            tiles[(x, y)] = tile_id
+        if tile_id == 4:
+            ballx = x
+        elif tile_id == 3:
+            paddlex = x
         elif x == -1:
-            current_score = tile_id
-        pos += 3
-    return tiles
-
-def get_score(instructions):
-    tiles = {}
-    pos = 0
-    while pos < len(instructions):
-        x = instructions[pos]
-        y = instructions[pos + 1]
-        tile_id = instructions[pos + 2]
-        if not x == -1:
+            score = tile_id
+        if not x == -1 and get_tiles:
             tiles[(x, y)] = tile_id
-        elif x == -1:
-            current_score = tile_id
         pos += 3
-    return current_score
+    return (ballx, paddlex, score, tiles)
 
-def show_grid(instructions):
-    tiles = read_instructions(instructions)
-    # print(len(block_tiles))
-    # print(block_tiles)
-
-    # assert len(block_tiles) == 239
-
-    # display the grid
+def show_grid(tiles):
     lookup_table = {0: ' ', 1: 'W', 2: 'B', 3: '-', 4: 'O'}
     display_grid(tiles, lookup_table)
 
-def get_ball(instructions):
-    tiles = read_instructions(instructions)
-    for key, value in tiles.items():
-        if value == 4:
-            ball = key
-    return ball
-
-def get_paddle(instructions):
-    tiles = read_instructions(instructions)
-    for key, value in tiles.items():
-        if value == 3:
-            paddle = key
-    return paddle
-
-def count_blocks(instructions):
-    tiles = read_instructions(instructions)
+def count_blocks(tiles):
     block_tiles = [key for key, value in tiles.items() if value == 2]
     return len(block_tiles)
 
-while True:
-    prog.run(reset=False)
-    ball = get_ball(prog.output)
-    paddle = get_paddle(prog.output)
-    n = count_blocks(prog.output)
-    if ball[0] < paddle[0]:
-        prog.run(input=-1, reset=False)
-        n_prev = n
-        n = count_blocks(prog.output)
-        if n != n_prev:
-            show_grid(prog.output)
-            print("blocks left:", n)
-            print("score:", get_score(prog.output))
-    elif ball[0] > paddle[0]:
-        prog.run(input=1, reset=False)
-        n_prev = n
-        n = count_blocks(prog.output)
-        if n != n_prev:
-            show_grid(prog.output)
-            print("blocks left:", n)
-            print("score:", get_score(prog.output))
-    elif ball[0] == paddle[0]:
-        prog.run(input=0, reset=False)
-        n_prev = n
-        n = count_blocks(prog.output)
-        if n != n_prev:
-            show_grid(prog.output)
-            print("blocks left:", n)
-            print("score:", get_score(prog.output))
-    if n == 0:
-        break
+startTime = time.time()
 
-print("score:", get_score(prog.output))
+prog.run(reset=False)
+ballx, paddlex, score, tiles = read_instructions(prog.output, get_tiles=False)
+
+while True:
+    if ballx < paddlex:
+        output = prog.run(input=-1, reset=False)
+    elif ballx > paddlex:
+        output = prog.run(input=1, reset=False)
+    elif ballx == paddlex:
+        output = prog.run(input=0, reset=False)
+    old_score = score
+    ballx, paddlex, score, tiles = read_instructions(prog.output, get_tiles=False)
+    if score != old_score:
+        ballx, paddlex, score, tiles = read_instructions(prog.output, get_tiles=True)
+        #show_grid(tiles)
+        n = count_blocks(tiles)
+        print("blocks left:", n)
+        #print("score:", score)
+        if n == 0:
+            break
+
+print("score:", score)
+
+executionTime = (time.time() - startTime)
+print('Execution time in seconds: ' + str(executionTime))
+
+
