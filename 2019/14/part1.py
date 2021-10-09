07a3440dc97e215af1ff5a376ae4b8bd4099d7b4
Lannie <rsl.ligthart@gmail.com>
2021-10-09 23:05:29 +0200

    works for ex 1-4 but not 5

--- 2019/14/part1.py
+++ 2019/14/part1.py
@@ -20,6 +20,22 @@ class Chemical():
     def __str__(self):
         return(str(self.n) + " " + self.type)
 
+    @property
+    def cost(self):
+        for key, value in formulas.items():
+            if self.type in key:
+                # convert strings to chemicals
+                unit_cost = [Chemical.parse(v) for v in value]
+                n_returned = int(key.split(" ")[0])
+                # multiply each chemical by factor needed, if amount needed > unit of production (otherwise example 3 wont'work)
+                if self.n > n_returned:
+                    factor = ceil(self.n / n_returned)
+                else:
+                    factor = 1
+                multiplied_cost = [c.multiply(factor) for c in unit_cost]
+                # units of production
+                return multiplied_cost
+
     @staticmethod
     def parse(string):
         n, type = string.split(" ")
@@ -40,6 +56,20 @@ def get_base_elements(formulas):
             base_elements.append(element)
     return base_elements
 
+def deduplicate(cost):
+    admin = {}
+    for chem in cost:
+        n, type = str(chem).split(" ")
+        n = int(n)
+        if type in admin:
+            admin[type] += n
+        else:
+            admin[type] = n
+    cost_updated = []
+    for key, value in admin.items():
+        cost_updated.append(Chemical(type=key, n=value))
+    return cost_updated
+
 with open('testinput.txt') as f:
     data = f.read().split("\n")
 
@@ -52,88 +82,56 @@ print("\n")
 
 # bepaal wat de basiselementen zijn (A en B)
 base_elements = get_base_elements(formulas)
-base_formulas = {}
 
-# bepaal de formules voor de basiselementen
-for be in base_elements:
-    for key, value in formulas.items():
-        if be == key.split(" ")[1]:
-            base_formulas[key] = value[0]
+# startpunt
+cost = [Chemical.parse("1 FUEL")]
 
-print(base_formulas)
-
-
-
-print("base elements are:")
-print(base_elements, "\n")
-
-# bepaal hoeveel je van elk basiselement nodig hebt
-def get_cost(element, factor):
-    # lookup of cost in formula dictionary
-    # multiply by factor specified
-    for key, value in formulas.items():
-        if element in key:
-            value = [Chemical.parse(v) for v in value]
-            value = [v.multiply(factor) for v in value]
-            value = [str(v) for v in value]
-            #print("the cost of ", factor, element, "is:")
-            #print(value)
-            return value
-
-
-# keep tabs of required base elements
-required = {}
-for e in base_elements:
-    required[e] = 0
-
-
-cost = []
-new_cost = ["1 FUEL"]
+stop = False
 while True:
-    # als alle elementen in cost een basiselement zijn runt de cyclus nog 1 keer en stopt dan.
+    # show current cost
+    cost = deduplicate(cost) # deze stap is nodig voor voorbeeld 4
+    print(" ".join([str(c) for c in cost]))
+    # Check of alle elementen in cost een basiselement zijn. Zo niet, dan moet de loop door blijven lopen.
     n_base = 0
     for chem in cost:
         if chem.type in base_elements:
             n_base += 1
-    # update cost
-    cost = [Chemical.parse(nc) for nc in new_cost]
-    print(" ".join([str(c) for c in cost]))
-    new_cost = []
-    for chem in cost:
-        if chem.type in base_elements:
-            new_cost.extend([str(chem)])
-        elif chem.type not in base_elements:
-            new_cost.extend(get_cost(chem.type, chem.n))
     if n_base == len(cost):
-        for c in cost:
-            required[c.type] += c.n
-        for key, value in required.items():
-            print(key, value)
+        stop = True  # indien True, dan stopt de loop aan het eind.
+    for i in range(len(cost)):
+        # indien geen base element, vervang door de cost van dit element en breek uit de for loop, die daarna weer
+        # bij het begin begint, waardoor elk element eerst helemaal wordt doorlopen tot het basiselement alvorens verder
+        # te gaan naar het volgende. Ik weet niet of dit de goede strategie is. Break weghalen maakt in elk geval wel
+        # voorbeeld 4 stuk.
+        if cost[i].type not in base_elements:
+            addition = cost[i].cost
+            cost.pop(i)
+            cost.extend(addition)
+            break
+    if stop:
         break
 
-
-
-# bepaal hoeveel ORE je nodig hebt om minimaal die hoeveelheid te maken van elk basiselement
-# 10/2 = 5 * 9 = 45 ORE voor 10 A
-# 23/3 = 8 * 8 = 64 ORE voor 23 B
-# 37/5 = 8 * 7 = 56 ORE voor 37 C
-
-amount_needed = 0
-
+final_cost = []
+required = {}
 for e in base_elements:
-    # get required
-    req = required[e]
-    # get per how many they are produced and how much ORE they cost
-    for key, value in base_formulas.items():
-        if e == key.split(" ")[1]:
-            per_n = int(key.split(" ")[0])
-            amount = int(value.split(" ")[0])
-            # divide required by unit of production and round
-            req_rounded = ceil(req/per_n)
-            amount_needed += req_rounded * amount
-            print(amount_needed)
+    required[e] = 0
+    
+cost = deduplicate(cost)
+for c in cost:
+    required[c.type] += c.n
+print("total required:")
+for key, value in required.items():
+    print(key, value)
+for key, value in required.items():
+    final_cost.append(Chemical(value, key))
 
+amount_needed = []
+for chem in final_cost:
+    amount_needed.extend(chem.cost)
+pass
 
-# tel deze bedragen op.
+n_ore = 0
+for chem in amount_needed:
+    n_ore += chem.n
 
-
+print(n_ore)
