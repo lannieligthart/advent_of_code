fb2f44da9785a580f66a68b41f40752551dfed2e
Lannie <rsl.ligthart@gmail.com>
2021-10-17 21:51:25 +0200

    day 15, part 1

--- 2019/15/part1.py
+++ 2019/15/part1.py
@@ -4,124 +4,141 @@ sys.path.append("C:/Users/Admin/SURFdrive/Code/advent_of_code/AoC_tools")
 import IntCode as ic
 from aoc_tools import display_grid
 
-code = ic.parse_code('C:/Users/Admin/SURFdrive/Code/advent_of_code/2019/15/input.txt')
-software = ic.Intcode(code)
-
-
-
 class RepairDroid(object):
 
     def __init__(self, software, position=[0, 0]):
         self.position = position
-        self.map = {(0,0): "D"}
+        self.map = {(0,0): "S"}
         self.software = software
-        self.previous_position = None
-        self.orientation = "N" # start with north facing orientation.
+        self.route = [(0,0)]
+        self.explored = [(0,0)]
+        self.oxygen_pos = None
+
+
+    def get_destination(self, direction):
+        # north
+        if direction == "N":
+            dest = self.position.copy()
+            dest[1] += 1
+        # east
+        elif direction == "E":
+            dest = self.position.copy()
+            dest[0] += 1
+        # south
+        elif direction == "S":
+            dest = self.position.copy()
+            dest[1] -= 1
+        # west
+        elif direction == "W":
+            dest = self.position.copy()
+            dest[0] -= 1
+        return tuple(dest)
 
     def move(self, direction):
         # take input
         # run IntCode
         # return new position (same or different depending on IntCode response)
-        print("Facing", self.orientation)
-        output = self.software.run(input=direction, halt_on_output=True, reset=False)
-        # if the droid can move, move it.
+        directions = {
+            "N": 1,
+            "S": 2,
+            "W": 3,
+            "E": 4
+        }
+        output = self.software.run(input=directions[direction], halt_on_output=True, reset=False)
         if output == 1 or output == 2:
-            self.map[tuple(self.position)] = "."
-            self.previous_position = self.position
+            # if droid can move, update self.position.
+            # Remove 'D' mark from old position.
+            if self.map[tuple(self.position)] != "S":
+                self.map[tuple(self.position)] = "."
+            # Update current position.
             # north
-            if direction == 1:
+            if direction == "N":
                 self.position[1] += 1
-                self.orientation = "N"
-                print("moving north, new position:", self.position)
-            # west
-            elif direction == 2:
-                self.position[0] -= 1
-                self.orientation = "W"
-                print("moving west, new position:", self.position)
-            # south
-            elif direction == 3:
-                self.position[1] -= 1
-                self.orientation = "S"
-                print("moving south, new position:", self.position)
+                #print("moving north, new position:", self.position)
             # east
-            elif direction == 4:
+            elif direction == "E":
                 self.position[0] += 1
-                self.orientation = "E"
-                print("moving east, new position:", self.position)
+                #print("moving east, new position:", self.position)
+            # south
+            elif direction == "S":
+                self.position[1] -= 1
+                #print("moving south, new position:", self.position)
+            # west
+            elif direction == "W":
+                self.position[0] -= 1
+                #print("moving west, new position:", self.position)
+            # mark new position on the map.
             self.map[tuple(self.position)] = "D"
-        # if it can't move, don't move but add the info to the map.
-        elif output == 0:
-            pos_to_add = self.get_destination(direction)
-            self.map[tuple(pos_to_add)] = "#"
-        # if the oxygen system has been found, return its position, otherwise None.
-        if output == 2:
-            print("Found the Oxygen system at position " + self.position + "!")
+            if self.position == (0,0):
+                return 99
         return output
 
-    def get_destination(self, direction):
-        # north
-        if direction == 1:
-            dest = self.position.copy()
-            dest[1] += 1
-        # west
-        elif direction == 2:
-            dest = self.position.copy()
-            dest[0] -= 1
-        # south
-        elif direction == 3:
-            dest = self.position.copy()
-            dest[1] -= 1
-        # east
-        elif direction == 4:
-            dest = self.position.copy()
-            dest[0] += 1
-        return tuple(dest)
 
-    def scan_and_move(self):
-        # order of directions
-        order_of_directions = [1, 2, 3, 4]
+    def scan_and_move(self, lookup):
         # which one we try first depends on orientation. We go straight, then right, etc.
-        if self.orientation == "N":
-            directions = order_of_directions
-        elif self.orientation == "W":
-            directions = [2, 3, 4, 1]
-        elif self.orientation == "S":
-            directions = [3, 4, 1, 2]
-        elif self.orientation == "E":
-            directions = [4, 1, 2, 3]
-        attempt = 0
+        directions = ["N", "E", "S", "W"]
         for d in directions:
-            attempt += 1
-            cur_pos = self.position.copy()
-            # determine destination and only try it if we haven't been there before, or if we've already
-            # tried all other options.
-            new_pos = self.get_destination(d)
-            if new_pos not in self.map and attempt <= 4:
-                output = droid.move(d)
-                # if output == 0 (can't go there), continue.
-                # if output == 1 or output == 2 (moved), exit loop prematurely because we need to start a new scan.
-                if output == 1 or output == 2:
-                    return output
-            elif attempt == 5:
-                new_pos = self.previous_position
-                self.previous_position = self.position
-                self.position = new_pos
-                return 0
+            # determine destination and only check it out if it's unexplored.
+            dest = self.get_destination(d)
+            if dest not in self.explored and (dest not in self.map or self.map[dest] != "#"):
+                output = self.move(direction=d)
+                # add collected info to the map.
+                # if inaccessible, add a "#"
+                if output == 0:
+                    self.map[tuple(dest)] = "#"
+                elif output == 1:
+                    self.route.append(tuple(dest))
+                    self.explored.append(tuple(dest))
+                    #display_grid(droid.map, lookup)
+                    return 1
+                elif output == 2:
+                    self.route.append(tuple(dest))
+                    self.map[tuple(dest)] = "O"
+                    self.explored.append(tuple(dest))
+                    self.oxygen_pos = dest
+                    return 2
+        # if no new direction to move was found, go back to previous position.
+        #print("no options, moving back to previous position!")
+        # based on route, figure out which direction to go back to.
+        current_pos = self.route[-1]
+        previous_pos = self.route[-2]
+        dif = [0, 0]
+        dif[0] = current_pos[0] - previous_pos[0]
+        dif[1] = current_pos[1] - previous_pos[1]
+        dif_dir = {
+            (1,0): "W",
+            (-1,0): "E",
+            (0,1): "S",
+            (0,-1): "N"
+        }
+        if self.map[tuple(self.position)] != "O":
+            self.map[tuple(self.position)] = "."
+        self.move(dif_dir[tuple(dif)])
+        # remove the last element from the route.
+        self.route.pop()
+        self.map[tuple(self.position)] = "D"
 
-
-
+code = ic.parse_code('C:/Users/Admin/SURFdrive/Code/advent_of_code/2019/15/input.txt')
+software = ic.Intcode(code)
 
 droid = RepairDroid(software)
 output = None
 
-while not output == 2:
-    output = droid.scan_and_move()
-    print(droid.position)
-    lookup = {"#": "#",
-              ".": ".",
-              "D": "D"}
-    display_grid(droid.map, lookup)
+lookup = {
+    "#": "#",
+    ".": ".",
+    "D": "D",
+    "S": "S",
+    "O": "O"
+}
 
-# algoritme schrijven voor het zoeken naar de oxygen system.
-# check welke zijden al bekend zijn, probeer te bewegen naar de eerstvolgende zijde die nog niet bekend is.
-# ga altijd eerst linksaf, tenzij je daar al geweest bent. Dan de eerstvolgende (clockwise).
+while not output == 2:
+    output = droid.scan_and_move(lookup)
+
+display_grid(droid.map)
+print(droid.oxygen_pos)
+print(droid.route)
+# kortste route:
+print(len(droid.route)-1)
+
+assert len(droid.route)-1 == 244
