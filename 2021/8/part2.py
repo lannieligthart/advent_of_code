8f8083e7c8edfeed50db5a1539c73dca35f44bff
Lannie <rsl.ligthart@gmail.com>
2021-12-08 21:09:43 +0100

    2021/8 some minor improvements

--- 2021/8/part2.py
+++ 2021/8/part2.py
@@ -21,46 +21,40 @@ def run(input_line):
 
     # 0, 9 en 6 heben len(6). 6 is de enige die niet beide elementen van 1 in zich heeft.
     len6 = [i for i in input if len(i) == 6]
-
     for s in len6:
-        for char in numbers[1]:
-            if char not in s:
-                numbers[6] = s
-                len6.remove(s)
+        if not set(numbers[1]).issubset(set(s)):
+            numbers[6] = s
+            len6.remove(s)
+            break
 
     # 0 en 9 verschillen in d/e. In 4 zit alleen d maar niet e.
-    # dus degene die niet volledig overlapt met 4 in de letter die verschilt, is 0.
-    x = len6[0]
-    y = len6[1]
-    dif = []
-    for char in x:
-        if not char in y:
-            dif.append(char)
-    for char in y:
-        if not char in x:
-            dif.append(char)
+    # dus degene die niet volledig overlapt met 4 in de letter die verschilt, is 0, de ander is 9.
+    dif1 = set(len6[0]) - set(len6[1])
+    dif2 = set(len6[1]) - set(len6[0])
+    dif = list(dif1.union(dif2))
 
-    letter_not_in_4 = [l for l in dif if l not in numbers[4]]
+    # bepaal welke van dif er niet in 4 voorkomt (kan er maar 1 zijn dus wordt een list van len 1).
+    not_in_4 = [l for l in dif if l not in numbers[4]][0]
     for s in len6:
-        if letter_not_in_4[0] in s:
+        if not_in_4 in s:
             numbers[0] = s
-        elif letter_not_in_4[0] not in s:
+        elif not_in_4 not in s:
             numbers[9] = s
 
     # 2 is degene van len(5) die niet volledig overlapt met 9.
     len5 = [i for i in input if len(i) == 5]
     for s in len5:
-        for char in s:
-            if char not in numbers[9]:
-                numbers[2] = s
-                len5.remove(s)
+        if not set(s).issubset(set(numbers[9])):
+            numbers[2] = s
+            len5.remove(s)
+            break
 
     # 3 en 5 zijn nu over. 3 is degene die niet volledig overlapt met 6.
     for s in len5:
-        for char in s:
-            if char not in numbers[6]:
-                numbers[3] = s
-                len5.remove(s)
+        if not set(s).issubset(set(numbers[6])):
+            numbers[3] = s
+            len5.remove(s)
+            break
 
     # 5 is degene die overblijft.
     numbers[5] = len5[0]
