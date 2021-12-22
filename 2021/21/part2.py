956184106b7144a30ee7b5f00c4d854bc5c72a91
Lannie <rsl.ligthart@gmail.com>
2021-12-22 13:45:39 +0100

    2021/21 works but slow

--- 2021/21/part2.py
+++ 2021/21/part2.py
@@ -7,6 +7,8 @@ for i in [1, 2, 3]:
 
 states = {}
 
+modulo10 = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 1]
+
 class Game(object):
 
     def __init__(self, space1, score1, space2, score2):
@@ -18,31 +20,27 @@ class Game(object):
     def __str__(self):
         return (f"Player 1 score: {self.score1}\nPlayer 2 score: {self.score2}")
 
+    def play(self, roll1, roll2):
+        space1 = modulo10[self.space1 + sum(roll1)]
+        score1 = self.score1 + space1
+        space2 = modulo10[self.space2 + sum(roll2)]
+        score2 = self.score2 + space2
+        return Game(space1, score1, space2, score2)
+
     def play1(self, roll):
         # 1 beurt bestaat uit:
         # 1 roll
         # space updaten
         # score updaten op basis van space
-        self.space1 += sum(roll)
-        if self.space1 > 10:
-            if self.space1 % 10 == 0:
-                self.space1 = 10
-            else:
-                self.space1 = self.space1 % 10
+        self.space1 = modulo10[self.space1 + sum(roll)]
         self.score1 += self.space1
 
     def play2(self, roll):
-        self.space2 += sum(roll)
-        if self.space2 > 10:
-            if self.space2 % 10 == 0:
-                self.space2 = 10
-            else:
-                self.space2 = self.space2 % 10
+        self.space2 = modulo10[self.space2 + sum(roll)]
         self.score2 += self.space2
 
-
 states = {
-    (4, 0, 8, 0): 1
+    (10, 0, 6, 0): 1
 }
 
 p1_wins = 0
@@ -53,26 +51,21 @@ while len(states) > 0:
     for r1 in rolls:
         for r2 in rolls:
             for state, n in states.items():
-                game = Game(*state)
-                game.play1(roll=r1)
-                game.play2(roll=r2)
+                game = Game(*state).play(roll1=r1, roll2=r2)
                 newstate = (game.space1, game.score1, game.space2, game.score2)
-                if newstate in newstates:
+                if newstate[1] >= 21:
+                    p1_wins += n
+                elif newstate[3] >= 21:
+                    p2_wins += n
+                elif newstate in newstates:
                     newstates[newstate] += n
                 elif newstate not in newstates:
                     newstates[newstate] = n
     states = newstates.copy()
-    print(sum(states.values()))
-    for key, value in newstates.items():
-        if key[1] >= 21:
-            p1_wins += value
-            states.pop(key)
-        elif key[3] >= 21:
-            p2_wins += value
-            states.pop(key)
 
-    #print(states)
-
-    print("P1 wins:", p1_wins)
+    print("P1 wins:", int(p1_wins/27))
     print("P2 wins:", p2_wins)
     print(sum(states.values()))
+
+
+#306719685234774
