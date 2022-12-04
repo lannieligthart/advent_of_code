d107a6fd1891fe2ff4344b80cf9eb6ef2c5ae89c
lannieligthart <rsl.ligthart@gmail.com>
2022-12-04 06:28:48 +0100

    2022/4

---
+++ 2022/4/part1.py
@@ -0,0 +1,30 @@
+import AoC_tools.aoc_tools as aoc
+
+data = aoc.read_input("input.txt", "\n", ",")
+
+
+def s(d):
+    d = d.split("-")
+    d = [int(x) for x in d]
+    return d
+
+
+def check_contains(pair):
+    start1, stop1 = pair[0]
+    start2, stop2 = pair[1]
+    if start1 >= start2 and stop1 <= stop2:
+        return True
+    elif start2 >= start1 and stop2 <= stop1:
+        return True
+    else:
+        return False
+
+
+data = [list(map(s, d)) for d in data]
+
+total = 0
+for d in data:
+    if check_contains(d):
+        total += 1
+
+assert total == 534
---
+++ 2022/4/part2.py
@@ -0,0 +1,27 @@
+import AoC_tools.aoc_tools as aoc
+
+data = aoc.read_input("input.txt", "\n", ",")
+
+
+def s(d):
+    d = d.split("-")
+    d = [int(x) for x in d]
+    return d
+
+
+def check_overlaps(pair):
+    start1, stop1 = pair[0]
+    start2, stop2 = pair[1]
+    if stop1 < start2 or stop2 < start1:
+        return False
+    else:
+        return True
+
+
+data = [list(map(s, d)) for d in data]
+total = 0
+for d in data:
+    if check_overlaps(d):
+        total += 1
+
+assert total == 841
