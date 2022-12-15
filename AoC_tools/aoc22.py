a4c0af6898b78401646c5b5678b7e7449f3223b7
lannieligthart <rsl.ligthart@gmail.com>
2022-12-15 20:11:58 +0100

    added some hopefully useful new features

--- AoC_tools/aoc22.py
+++ AoC_tools/aoc22.py
@@ -66,12 +66,17 @@ def end(start_time):
     print('Execution time in seconds: ' + str(executionTime))
 
 
+def pad(str, n):
+    dif = n - len(str)
+    prefix = " "*dif
+    return prefix + str
+
 class Point(object):
 
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
-
+    def __init__(self, pos, pos2=None):
+        pos = Grid.parse_pos(pos, pos2)
+        self.x = pos[0]
+        self.y = pos[1]
 
     def __str__(self):
         return f"""Position: ({self.x}, {self.y})"""
@@ -80,17 +85,102 @@ class Point(object):
     def pos(self):
         return (self.x, self.y)
 
-class Grid(object):
-    # grid has x and y axis. In principle, x axis runs from low to high, y axis also, except if flip_y is
-    # specified. If positions are given as row, column, r/c should be reversed to x/y (specify matrix=True).
+    def mhd(self, other):
+        """manhattan distance"""
+        return abs(self.x - other.x) + abs(self.y - other.y)
 
-    # Grid() should create an object based on dictionary of positions and values. Use default values if no value is
-    # specified. If no dictionary is available, use Grid.make().
+class Vector(Point):
 
-    def __init__(self, points, values, na=" "):
+    def __add__(self, other):
+        x = self.x + other.x
+        y = self.y + other.y
+        return Vector(x, y)
+
+    def __sub__(self, other):
+        x = self.x - other.x
+        y = self.y - other.y
+        return Vector(x, y)
+
+    def __mul__(self, other):
+        # multiply by another vector
+        if isinstance(other, Vector):
+            x = self.x * other.x
+            y = self.y * other.y
+            return Vector(x, y)
+        # multiply by an int
+        elif isinstance(other, int):
+            x = self.x * other
+            y = self.y * other
+            return Vector(x, y)
+        else:
+            raise TypeError("Vector can only be multiplied by Vector or int")
+
+
+
+class Line(object):
+
+    def __init__(self, points):
+        """basically a series of points representing a horizontal or vertical line between two positions"""
+        # TODO: add support for diagonals
         self.points = points
-        self.values = values
+
+    @classmethod
+    def draw(cls, v1, v2):
+        """takes two positions (vectors or points? currently vectors only) and creates points at and between those positions"""
+        points = []
+        dif = v2 - v1
+        if dif.x > 0:
+            for i in range(dif.x + 1):
+                p = Point(v1.x + i, v1.y + 0)
+                points.append(p)
+        elif dif.x < 0:
+            for i in range(0, dif.x - 1, -1):
+                p = Point(v1.x + i, v1.y + 0)
+                points.append(p)
+        elif dif.y > 0:
+            for i in range(dif.y + 1):
+                p = Point(v1.x + 0, v1.y + i)
+                points.append(p)
+        elif dif.y < 0:
+            for i in range(0, dif.y - 1, -1):
+                p = Point(v1.x + 0, v1.y + i)
+                points.append(p)
+        return cls(points)
+
+class Grid(object):
+    """grid has x and y axis. Both x and y axis run from low to high"""
+
+    def __init__(self, points=dict(), values=dict(), na=" "):
+        self.points = points # dictionary with Point objects and positions as keys
+        self.values = values # dictionary with string or int values and positions as keys
         self.na = na
+
+    @classmethod
+    def from_dict(cls, dictionary):
+        """create a Grid based on a dictionary with positions as keys and values as dict values"""
+        points = dict()
+        values = dict()
+        for key, value in dictionary.items():
+            points[key] = Point(*key)
+            values[key] = value
+        grid = cls(points, values)
+        return grid
+
+    @classmethod
+    def from_list(cls, list, char="#"):
+        """takes a list of positions and creates a Grid that displays a specified character in the positions in the
+        list"""
+        points = dict()
+        values = dict()
+        for item in list:
+            if isinstance(item, Point):
+                points[item.pos] = item
+                values[item.pos] = char
+            else:
+                points[item] = Point(item)
+                values[item] = char
+        grid = cls(points, values)
+        return grid
 
     @staticmethod
     def read(data, rowsep='\n', colsep=" "):
@@ -207,6 +297,8 @@ class Grid(object):
 
     def display(self, transpose=False, show=True):
         """print the grid to console, with option to transpose it"""
+        lengths = [len(str(value)) for value in self.values.values()]
+        maxlen = max(lengths)
         if transpose:
             grid = self.df.T
         else:
@@ -214,13 +306,45 @@ class Grid(object):
         lol = grid.values.tolist()
         image = ""
         for l in lol:
+            # if values differ in length, pad with whitespace to make sure they align correctly
+            l = list(map(lambda x: pad(x, maxlen), l))
             image += " ".join(l) + "\n"
         if show:
             print(image)
         # return for testing purposes
-        return(image)
+        return image
 
-    def get_neighbour(self, pos):
+    @staticmethod
+    def parse_pos(pos, pos2=None):
+        # if pos is already a tuple, do nothing.
+        # if it's a point or an int, convert to tuple.
+        if isinstance(pos, Point):
+            pos = pos.pos
+        elif isinstance(pos, int) and isinstance(pos2, int):
+            pos = (pos, pos2)
+        elif isinstance(pos, tuple) and len(pos) == 2:
+            pass
+        elif isinstance(pos,list) and len(pos) == 2:
+            pos = list(pos)
+
+        else:
+            raise ValueError("Invalid position given")
+        return pos
+
+    def get_value(self, pos, pos2=None):
+        pos = self.parse_pos(pos, pos2)
+        return self.values[pos]
+
+    def set_value(self, value, pos, pos2=None):
+        pos = self.parse_pos(pos, pos2)
+        self.values[pos] = value
+
+    def add(self, point, value="#"):
+        self.points[point.pos] = point
+        self.values[point.pos] = value
+
+    def get_neighbour(self, pos, pos2=None):
+        pos = self.parse_pos(pos, pos2)
         # before returning a point, check that it exists on the grid. If not, return None.
         if pos in self.values.keys():
             return Point(*pos)
@@ -270,9 +394,11 @@ class Grid(object):
         W = self.get_west(point)
         NW = self.get_northwest(point)
         if n == 4:
-            return [N, S, E, W]
+            all_nb = [N, S, E, W]
+            return [nb for nb in all_nb if nb is not None]
         elif n == 8:
-            return [N, NE, E, SE, S, SW, W, NW]
+            all_nb = [N, NE, E, SE, S, SW, W, NW]
+            return [nb for nb in all_nb if nb is not None]
         else:
             raise ValueError("Invalid number of neighbours specified! Specify 4 or 8.")
 
@@ -290,251 +416,10 @@ class Grid(object):
             west.append(Point(point.x, y))
         for y in range(point.y+1, self.y_max + 1):
             east.append(Point(point.x, y))
-        return (north, east, south, west)
+        return north, east, south, west
 
-
-
-
-# class Grid(object):
-#
-#     def __init__(self, positions, empty=' ', reverse=False, lookup_table=None, matrix=False):
-#         """takes positions formatted in grid as (x, y). If positions are provided as (row,col),
-#         all positions should be positive."""
-#         # positions should be a dictionary with coordinates and corresponding values.
-#         self.points = {key: Point(key, value) for key, value in positions.items()}
-#         self.lookup_table = lookup_table
-#         self.empty = empty
-#         self.matrix = matrix
-#
-#
-#     @staticmethod
-#     def make(data, rowsep='\n', colsep=" "):
-#         """takes a list of lists, a list of strings, or a single string and returns a grid"""
-#         #print("\ninput:")
-#         #print(data, "\n")
-#         if isinstance(data, str):
-#             # if data is a single string, it should be converted to a list of lists using the separators.
-#             data_new = []
-#             rows = data.split(rowsep)
-#             for row in rows:
-#                 row = row.split(colsep)
-#                 # If row elements are not separated, split them
-#                 if len(row) == 1:
-#                     row = [char for char in row[0]]
-#                 data_new.append(row)
-#             data = data_new
-#             #data = [row.split(colsep) for row in rows]
-#         elif isinstance(data, list):
-#             # if the elements of the lists are not lists themselves, split them up into lists.
-#             if isinstance(data[0], str):
-#                 data = [row.split(colsep) for row in data]
-#         positions = {}
-#         # rows
-#         for row in range(len(data)):
-#             # columns
-#             for col in range(len(data[row])):
-#                 positions[(col, row)] = data[row][col]
-#         return Grid(positions, matrix=True)
-#
-#     @property
-#     def x_min(self):
-#         """lowest value on x-axis"""
-#         x_values = [point[0][0] for point in self.points.items()]
-#         return min(x_values)
-#
-#     @property
-#     def x_max(self):
-#         """highest value on x-axis"""
-#         x_values = [point[0][0] for point in self.points.items()]
-#         return max(x_values)
-#
-#     @property
-#     def x_range(self):
-#         return (self.x_min, self.x_max)
-#
-#     @property
-#     def y_min(self):
-#         """lowest value on y-axis"""
-#         y_values = [point[0][1] for point in self.points.items()]
-#         return min(y_values)
-#
-#     @property
-#     def y_max(self):
-#         """highest value on y-axis"""
-#         y_values = [point[0][1] for point in self.points.items()]
-#         return max(y_values)
-#
-#     @property
-#     def y_range(self):
-#         return (self.y_min, self.y_max)
-#
-#     @property
-#     def dim(self):
-#         dim_x = self.x_max + abs(self.x_min) + 1
-#         dim_y = self.y_max + abs(self.y_min) + 1
-#         return (dim_x, dim_y)
-#
-#     @property
-#     def grid(self):
-#         # column indices always run from low to high
-#         cols = [i for i in range(self.x_min, self.x_max + 1)]
-#
-#         # in grid mode, the y-axis values go from high to low.
-#         if self.x_min < 0 or (self.x_min >= 0 and not self.matrix):
-#             index = [i for i in range(self.y_max, self.y_min - 1, -1)]
-#             if self.x_min >= 0 and not self.matrix:
-#                 print('Grid only has positive values; if it was specified as a matrix (r,c), specify "matrix=True" '
-#                       'for correct display.')
-#         # in matrix mode, the y-axis values go from low to high.
-#         elif self.matrix:
-#             index = [i for i in range(self.y_min, self.y_max + 1)]
-#
-#         df = pd.DataFrame(columns=cols, index=index)
-#
-#         # first, fill up with emtpy strings
-#         for col in df.columns:
-#             df[col].values[:] = self.empty
-#
-#         if self.lookup_table is None:
-#             for point in self.points.values():
-#                 df.loc[point.position[0], point.position[1]] = str(point.value)
-#
-#         elif self.lookup_table is not None:
-#             for p in self.points:
-#                 for key, value in self.lookup_table.items():
-#                     if p.position == key:
-#                         df.loc[p[0], p[1]] = str(value)
-#         return df
-#
-#     def display(self, transpose=False, show=True):
-#         """print the grid to console, with option to transpose it"""
-#         if transpose:
-#             grid = self.grid.T
-#         else:
-#             grid = self.grid
-#         lol = grid.values.tolist()
-#         image = ""
-#         for l in lol:
-#             image += " ".join(l) + "\n"
-#         if show:
-#             print(image)
-#         return(image)
-
-# TODO: integrate Point class in Grid, so a Grid contains Points instead of positions.
-
-# class Point():
-#
-#     def __init__(self, pos):
-#         self.row = pos[0]
-#         self.col = pos[1]
-#         try:
-#             self.value = int(value)
-#         except ValueError:
-#             self.value = value
-#
-#     def __str__(self):
-#         return f"({self.row}, {self.col}): {self.value}"
-#
-#     @property
-#     def position(self):
-#         return (self.row, self.col)
-#
-#     @property
-#     def N(self):
-#         return (self.row - 1, self.col)
-#
-#     @property
-#     def S(self):
-#         return (self.row + 1, self.col)
-#
-#     @property
-#     def W(self):
-#         return (self.row, self.col - 1)
-#
-#     @property
-#     def E(self):
-#         return (self.row, self.col + 1)
-#
-#
-#     @property
-#     def NE(self):
-#         return (self.row - 1, self.col + 1)
-#
-#     @property
-#     def SE(self):
-#         return (self.row + 1, self.col + 1)
-#
-#     @property
-#     def NW(self):
-#         return (self.row + 1, self.col - 1)
-#
-#     @property
-#     def SW(self):
-#         return (self.row - 1, self.col - 1)
-#
-#
-#     def get_neighbours(self, grid):
-#         """obtain a point's neighbours' coordinates and values based on grid info.
-#         First gets the neighbour's position based on NESW properties, then retrieves the values from
-#         grid.positions, which is a dictionary of positions with their corresponding values."""
-#         neighbours = []
-#         #print("neighbours:")
-#         try:
-#             neighbour_S = Point(self.S, grid.points[self.S].value)
-#             #print("South: ", neighbour_S)
-#             neighbours.append(neighbour_S)
-#         except KeyError as e:
-#             pass
-#         try:
-#             neighbour_E = Point(self.E, grid.points[self.E].value)
-#             #print("East: ", neighbour_E)
-#             neighbours.append(neighbour_E)
-#         except KeyError as e:
-#             pass
-#         try:
-#             neighbour_N = Point(self.N, grid.points[self.N].value)
-#             #print("North: ", neighbour_N)
-#             neighbours.append(neighbour_N)
-#         except KeyError as e:
-#             pass
-#         try:
-#             neighbour_W = Point(self.W, grid.points[self.W].value)
-#             #print("West: ", neighbour_W)
-#             neighbours.append(neighbour_W)
-#         except KeyError as e:
-#             pass
-#         return neighbours
-#
-#
-#
-#     def get_8neighbours(self, grid):
-#         """obtain a point's neighbours' coordinates and values based on grid info.
-#         First gets the neighbour's position based on NESW properties, then retrieves the values from
-#         grid.positions, which is a dictionary of positions with their corresponding values."""
-#         neighbours = self.get_neighbours(grid)
-#         #print("neighbours:")
-#         try:
-#             neighbour_NE = Point(self.NE, grid.points[self.NE].value)
-#             #print("South: ", neighbour_NE)
-#             neighbours.append(neighbour_NE)
-#         except KeyError as e:
-#             pass
-#         try:
-#             neighbour_SE = Point(self.SE, grid.points[self.SE].value)
-#             #print("East: ", neighbour_SE)
-#             neighbours.append(neighbour_SE)
-#         except KeyError as e:
-#             pass
-#         try:
-#             neighbour_SW = Point(self.SW, grid.points[self.SW].value)
-#             #print("North: ", neighbour_SW)
-#             neighbours.append(neighbour_SW)
-#         except KeyError as e:
-#             pass
-#         try:
-#             neighbour_NW = Point(self.NW, grid.points[self.NW].value)
-#             #print("West: ", neighbour_NW)
-#             neighbours.append(neighbour_NW)
-#         except KeyError as e:
-#             pass
-#         return neighbours
+    def add_line(self, line, char="#"):
+        """takes a Line object and adds its points to the grid"""
+        for point in line.points:
+            self.points[point.pos] = point
+            self.values[point.pos] = char
