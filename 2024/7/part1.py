7b311f407987df0ad37a289c96c511d75c8d5839
Lannie Ligthart <rsl.ligthart@gmail.com>
2024-12-07 12:05:55 +0100

    2024/7 veeeeeeeeery slow but works

---
+++ 2024/7/part1.py
@@ -0,0 +1,47 @@
+from AoC_tools import aoc22 as aoc
+import itertools
+
+s = aoc.start()
+infile = "testinput.txt"
+infile = "input.txt"
+
+with open(infile) as f:
+    data = f.read().split("\n")
+
+data = [d.split() for d in data]
+
+eq = dict()
+
+for d in data:
+    eq[int(d[0].replace(":", ""))] = d[1:]
+
+def try_combo(combo, numbers):
+    answer = numbers[0]
+    for i in range(len(numbers) - 1):
+        eq = str(answer) + combo[i] + numbers[i+1]
+        #print(eq)
+        answer = eval(eq)
+    return answer
+
+total = 0
+
+for key, value in eq.items():
+    combos = itertools.product(["*", "+"], repeat=len(value) - 1)
+    answers = []
+    for combo in combos:
+        answer = try_combo(combo, value)
+        if answer == key:
+            total += key
+            break
+
+print(total)
+
+if infile == "testinput.txt":
+    assert total == 3749
+
+if infile == "input.txt":
+    assert total == 932137732557
+
+
+print(aoc.end(s))
+
